14a55bba7808ae98f29024d56dcd1a55b94cf41f
Matt Jones <mattjones444@gmail.com>
2024-09-15 20:41:02 +0000

    Added details to homepage

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,3 +1,9 @@
 $('.carousel').carousel({
     interval: 5000 // Adjust the interval as needed
 });
+document.addEventListener('DOMContentLoaded', function() {
+    // Delay the AOS initialization by 1 second (1000 milliseconds)
+    setTimeout(function() {
+        AOS.init();
+    }, 2000); // 1000 milliseconds = 1 second
+});
